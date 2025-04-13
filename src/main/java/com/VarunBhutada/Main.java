9f7aa1d5708da7f02fa46c5bb47b4b995eb59610
va-run-6626 <vbhutada143@gmail.com>
2025-04-13 12:58:12 +0530

    updated password passing functionality

--- src/main/java/com/VarunBhutada/Main.java
+++ src/main/java/com/VarunBhutada/Main.java
@@ -15,6 +15,14 @@ public class Main {
             int emailCol = Integer.parseInt(prop.getProperty("email.column"));
             int statusCol = Integer.parseInt(prop.getProperty("status.column"));
 
+            String emailPassword = System.getProperty("EMAIL_PASSWORD");
+            if(emailPassword == null) {
+                emailPassword = System.getenv("EMAIL_PASSWORD");
+            }
+            if(emailPassword == null) {
+                emailPassword = prop.getProperty("email.password");
+            }
+
             // Process Excel
             ExcelProcessor processor = new ExcelProcessor();
             List<String> unregistered = processor.getUnregisteredEmails(excelPath, emailCol, statusCol);
@@ -24,7 +32,7 @@ public class Main {
                 prop.getProperty("email.host"),
                 Integer.parseInt(prop.getProperty("email.port")),
                 prop.getProperty("email.from"),
-                prop.getProperty("email.password")
+                emailPassword
             );
 
             sender.sendReminders(unregistered);
